873bbb950a0bc6d3a21d1ca62e8bfedb9fca8429
wyj12 <wyj1254157112@gmail.com>
2023-12-04 17:56:32 -0500

    feat: implement rough HomePage layout 2023-12-04 5:56

---
+++ hotel-reservation/src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+
+const HomePage = () => {
+    return (
+        <Grid container spacing={1}>
+
+        </Grid>
+    );
+};
+
+export default HomePage;
