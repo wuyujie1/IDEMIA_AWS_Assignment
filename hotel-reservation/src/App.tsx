7df43f39323c1b57f3ac0510cbd7860829b5fe11
wyj12 <wyj1254157112@gmail.com>
2023-12-10 20:57:07 -0500

    feat: Add components to homepage and app page

--- hotel-reservation/src/App.tsx
+++ hotel-reservation/src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/HomePage";
-import SearchCriteria from "./components/SearchCriteria";
 import { ThemeProvider } from "@mui/material";
 import theme from "./theme";
 
--- hotel-reservation/src/pages/HomePage.tsx
+++ hotel-reservation/src/pages/HomePage.tsx
@@ -1,10 +1,20 @@
 import React from "react";
-import { Grid } from "@mui/material";
+import { Button, Grid } from "@mui/material";
+import SearchCriteria from "../components/SearchCriteria";
+import SearchResult from "../components/SearchResult";
+
 function HomePage() {
     return (
-        <Grid container spacing={1}>
-            <Grid item xs={12} lg={12}>
-                <h2>Hotel Reservation</h2>
+        <Grid container spacing={1}
+            sx={{margin: "2%", border: "1px solid lightgray", borderRadius: "10px", width: "90%"}}>
+            <Grid item xs={12} sm={10}>
+                <h1 style={{fontWeight: "lighter"}}>Reservations</h1>
+            </Grid>
+            <Grid item xs={12} sm={12} sx={{padding: "2%"}}>
+                <SearchCriteria/>
+            </Grid>
+            <Grid item xs={12} sm={12} sx={{padding: "2%"}}>
+                <SearchResult/>
             </Grid>
 
         </Grid>
