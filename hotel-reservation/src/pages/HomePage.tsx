dbe22c1657d0d5487e845c59d9366cf6a481eba4
wyj12 <wyj1254157112@gmail.com>
2023-12-10 20:58:37 -0500

    refactor: update homepage, remove unused imports

--- hotel-reservation/src/pages/HomePage.tsx
+++ hotel-reservation/src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Grid } from "@mui/material";
+import { Grid } from "@mui/material";
 import SearchCriteria from "../components/SearchCriteria";
 import SearchResult from "../components/SearchResult";
 
